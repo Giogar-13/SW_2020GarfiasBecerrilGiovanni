bafa2f3bdc2c4949af9e9a18b1820b060099fe11
Giogar-13 <giogar13@comunidad.unam.mx>
2020-06-01 16:39:40 -0500

    FEAT: se agregó Actividad2SW.php

---
+++ Actividad2SW.php
@@ -0,0 +1,12 @@
+<?php
+
+ echo "Garfias Becerril Giovanni";
+
+ echo "He aprendido qué son las cookies, y en verdad me ha gustado porque siempre veía el concepto y nunca supe qué era.";
+
+ echo "También los formularios, está asombroso lo que puedes hacer en PHP con los datos de las personas. Y pues el manejo de las variables es súper importante.";
+
+
+
+  ?>
+0 comments on commit 74d7
