8b1c3503da934b93470016cdc135cedb4ee055d0
Giogar-13 <giogar13@comunidad.unam.mx>
2020-06-04 17:36:45 -0500

    FEAT: se modificaron los archivos de la carpeta Garfias.Actividad7 correspondientes a las nuevas especificaciones de la actividad 7.2

--- Garfias.Actividad7/dynamics/validacion.php
+++ Garfias.Actividad7/dynamics/validacion.php
@@ -3,17 +3,26 @@ $RFC = (isset($_POST['RFC']) && $_POST['RFC'] != "")? $_POST['RFC'] : false;
 
 $nombre = (isset($_POST['nombre']) && $_POST['nombre'] != "")? $_POST['nombre'] : false;
 
+$contra = (isset($_POST['contrasena']) && $_POST['contrasena'] != "")? $_POST['contrasena'] : false;
 
-if ($RFC != false && $nombre != false)
+
+if ($RFC != false && $nombre != false && contrasena)
 {
+  $validacionContra = preg_match_all('/^((?=\S*[A-Z])(?=\S*[a-z])(?=\S*\d)(?=\S*[@$&%#!¡?¿.,]))\S{10,30}$/', $contra);
+
   $validacionRFC = preg_match_all('/[A-Za-z]{4}[0-9][0-9](0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([0-9]|[A-Za-z]){3}/', $RFC);
 
   $validacionNombre = preg_match_all('/[A-za-z]{3,25}\s?([A-za-z]{3,25})?/',$nombre);
 
-  if($validacionRFC && $validacionNombre)
+  if($validacionRFC && $validacionNombre && $validacionContra)
   {
     echo "Tus datos fueron ingresados correctamente.";
   }
+  elseif ($validacionContra == false)
+  {
+    echo "Tu contraseña '".$contra."' no es lo suficientemente segura, vuelve y escríbe una que cumpla con las características.";
+  }
+
   elseif ($validacionRFC == false)
   {
     echo "Has escrito incorrectamente tu RFC";
